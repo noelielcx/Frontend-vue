5b88ae1d92e7d0cfc673a59228c99229242419c8
Noëlie Lacroix <noelielacroix@orange.fr>
2023-02-06 08:26:48 +0000

    Update 3 files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,6 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/categories">Categories</RouterLink>
+        <RouterLink to="/produits">Produits</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/ProduitView.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div>
+      <!-- Un formulaire pour saisir les valeurs de la catégorie à ajouter -->
+      <form @submit.prevent="ajouteProduit"></form>
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+function ajouteProduit() {
+// Ajouter une catégorie avec les données du formulaire
+const options = {
+    method: "POST", // Verbe HTTP POST pour ajouter un enregistrement
+    body: JSON.stringify(data.formulaireCategorie),
+    headers: {
+        "Content-Type": "application/json",
+    },
+};
+</script>
